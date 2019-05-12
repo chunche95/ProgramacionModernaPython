b759b5f83261cab3d1cccf44e2c3f9fb47d5134d
chunche95 <paeste95.pb@gmail.com>
2019-05-12 13:10:37 +0200

    Cambios del 12 de May ,2019 -  Funciones De primer Nivel (1) Funcion de primer nivel con return

---
+++ programacion_moderna_python-chunche/funcionesPrimerNivelConReturn.py
@@ -0,0 +1,33 @@
+def maxi(*l): # Entrada de parámetros 
+    if len(l) == 0:
+        return 0
+    m= l[0]
+    for ix in range(1, len(l)):
+        if l[ix] > m:
+            m = l[ix]
+    return m
+def mini(*l): # Entrada de parámetros 
+    if len(l) == 0:
+        return 0
+    m= l[0]
+    for ix in range(1, len(l)):
+        if l[ix] < m:
+            m = l[ix]
+    return m
+
+def media(*l):
+    if len(l)==0:
+        return 0
+    suma = 0
+    for valor in l:
+        suma += valor
+    return suma / len(l)
+funciones={
+    'max':maxi,
+    'min':mini,
+    'med':media
+    }
+def returnF(nombre):
+    if nombre.lower() in funciones.keys():
+        return funciones[nombre]
+    return None

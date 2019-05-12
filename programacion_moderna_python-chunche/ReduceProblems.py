870025c2190b588c463f0710da1a36ddfddb2134
chunche95 <paeste95.pb@gmail.com>
2019-05-12 18:33:35 +0200

    Cambios del 12 de May ,2019 -  Reduces problems elemento distinto e indices de listas (1)

---
+++ programacion_moderna_python-chunche/ReduceProblems.py
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+def SumatorioDobleClasico(l):
+    resultado = 0
+    for valor in l:
+        resultado += valor *2
+    return resultado
+
+def ProductoTotal(l):
+    resultado=1
+    for valor in range l:
+        resultado *=valor
+    return resultado
+
+lista = [1,4,56,8,-4]
+
+sumatorio = reduce(lambda x,y: x + y, lista)
+# Creo una copia de la lista
+l = lista[:]
+# Añado el neutro para la sima en la posición cero.
+l.insert(0,0)
+sumatorioDobles = reduce(lambda x,y: x+y*2,l)
+
+print(sumatorio)
+print(sumatorioDobles)
+
+
+
+
+
+

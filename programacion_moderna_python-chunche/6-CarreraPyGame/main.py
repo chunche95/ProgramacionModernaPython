4e5b90af3836e29afd8ecdbb33bd3313adb04c87
chunche95 <paeste95.pb@gmail.com>
2019-05-20 00:55:13 +0200

    Creacion en pygame de Race of Objects

--- programacion_moderna_python-chunche/6-CarreraPyGame/main.py
+++ programacion_moderna_python-chunche/6-CarreraPyGame/main.py
@@ -12,19 +12,57 @@
             - renderizar la pantalla:
 """
 # @Author: Chunche
-
+#
 
 import pygame
+import sys
+
+class runner():
+    pass 
 
 class Game():
+    corredores = []
     
     def __init__(self):
         self.__screen = pygame.display.set_mode((640,480))
         pygame.display.set_caption("Carrera de tortuguas")
         # Creacion de atributo
-        self.background = pygame.image.load("img/bg.png")
+        self.background = pygame.image.load("img/bg1.png")
+        self.runner = pygame.image.load("img/6.png")
+        
+    
+        
+    def competir(self):
+        x = 0
+        ganador = False
+        while True:
+            # Comprobación de los eventos.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            # Refresacar / renderizar la pantalla.
+            self.__screen.blit(self.background, (0,0))
+            self.__screen.blit(self.runner, (x,240))
+            pygame.display.flip()
+            
+            x += 3
+            if x >= 250:
+                ganador = True
+        
+        pygame.quit()
+        sys.exit()
         
 if __name__ == '__main__':
     # Inicializamos el paquete
     pygame.init()
     game = Game()
+    game.competir()
+    
+    
+    
+    
+    
+    
+    
+    

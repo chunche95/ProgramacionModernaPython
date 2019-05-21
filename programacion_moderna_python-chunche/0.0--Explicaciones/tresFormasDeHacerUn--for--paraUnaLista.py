7aecc517a5e4a895f08418dd8c4051c7cac363f5
chunche95 <paeste95.pb@gmail.com>
2019-05-21 15:16:29 +0200

    Comporbacion de eventos (1)

---
+++ programacion_moderna_python-chunche/0.0--Explicaciones/tresFormasDeHacerUn--for--paraUnaLista.py
@@ -0,0 +1,16 @@
+#
+#
+#
+lista=("Pantalla","Teclado","Raton","Disco","LED's","USB","Altavoces")
+# Primero --> Conozco la longitud de la lista
+for i in range (0,4):
+    print(lista[i])
+print ("+++++++++++")
+# Segundo --> Llamo a la sentencia de la longitud para que cuente la lista de objetos
+for i in range ( 0, len(lista)):
+    print(lista[i])
+print ("+++++++++++")
+# Tercera --> Llamo la lista, se cuenta y se imprime todo en conjunto
+for regalo in lista:
+    print(regalo)
+print ("+++++++++++")
--- programacion_moderna_python-chunche/6-CarreraPyGame/main.py
+++ programacion_moderna_python-chunche/6-CarreraPyGame/main.py
@@ -19,16 +19,19 @@ import sys
 import random
 
 class Runner():
-    def __init__(self, x=0, y=0):
-        self.custome = pygame.image.load("img/2.png")
+    __customes = ("1","2","3","4","5",)
+    def __init__(self, x=0, y=0, custome='2'):
+        self.custome = pygame.image.load("img/{}.png".format(custome))
         self.position = [x,y]
-        self.name = "Tortugui"
+        self.name = custome
     def avanzar(self):
         self.position[0] += random.randint(1,4) 
         
 class Game():
     runners = []
-    __startLine = -10
+    __posY = (160,200,240,280)
+    __names = ("Nemo","Ninja", "Speedy Fast", "Octopus")
+    __startLine = -5
     __finishLine = 620
     
     def __init__(self):
@@ -37,16 +40,17 @@ class Game():
         self.__background = pygame.image.load("img/bg1.png")
         pygame.display.set_caption("Carrera de tortuguas")
         
-        
-        firstRunner = Runner(self.__startLine,240)
-        firstRunner.name = "Speedy Fast"
-        
-        #self.runners = pygame.image.load("img/6.png")
-        self.runners.append(firstRunner)
+        for i in range(4):
+            theRunner = Runner(self.__startLine,self.__posY[i])
+            theRunner.name = self.__names[i]
+            #self.runners = pygame.image.load("img/6.png")
+            self.runners.append(theRunner)
         
         #runners.append(firstRunner)
         #runners.append(Runner(self.__startLine, 240))
-    
+    def close(self):
+        pygame.quit()
+        sys.exit()
         
     def competir(self):
         # x = 0
@@ -58,23 +62,30 @@ class Game():
                     gameOver = True
             
             # Avance del corredor - Creamos el metodo para ello llamamos a avanzar()
-            self.runners[0].avanzar()
-            if self.runners[0].position[0] >= self.__finishLine:
-                print ("{} win!".format(self.runners[0].name))
-                gameOver = True
+            for activeRunner in self.runners:
+                activeRunner.avanzar()
+                if activeRunner.position[0] >= self.__finishLine:
+                    print ("{} win!".format(activeRunner.name))
+                    gameOver = True
             
             
             # Refresacar / renderizar la pantalla.
             self.__screen.blit(self.__background, (0,0))
-            self.__screen.blit(self.runners[0].custome, self.runners[0].position)
+            
+            # Creacion de los 'n' corredores
+            for runner in self.runners:
+                self.__screen.blit(runner.custome, runner.position)
+                
+            # Inicializacion de la pantalla
             pygame.display.flip()
             
              # x += 3
              #if x >= 250:
              #   ganador = True
-        
-        pygame.quit()
-        sys.exit()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT():
+                    self.close()
         
 if __name__ == '__main__':
     # Inicializamos el paquete

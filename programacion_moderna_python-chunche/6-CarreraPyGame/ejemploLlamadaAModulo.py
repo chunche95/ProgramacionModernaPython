c1642879cd8a93af7c3ba06bd8fa2737626de342
chunche95 <paeste95.pb@gmail.com>
2019-05-21 19:10:48 +0200

    Explicacion de __main__

---
+++ programacion_moderna_python-chunche/6-CarreraPyGame/ejemploLlamadaAModulo.py
@@ -0,0 +1,3 @@
+import turtleMovement #  La variable  'name' del modulo importado  si se ejecuta como secundario nos indica el nombre del archivo 'turtleMovement',
+                      #  no como principal, el principal es este 'ejemploLlamadaAModulo.py'
+print("Hola, Chacho")

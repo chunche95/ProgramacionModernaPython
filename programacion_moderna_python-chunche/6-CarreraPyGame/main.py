6df8ecb655be1d7a475ced3296a15ec045edcd61
chunche95 <paeste95.pb@gmail.com>
2019-05-21 11:59:41 +0200

    Creacion de la clase corredor - pygame

--- programacion_moderna_python-chunche/6-CarreraPyGame/main.py
+++ programacion_moderna_python-chunche/6-CarreraPyGame/main.py
@@ -17,52 +17,61 @@
 import pygame
 import sys
 
-class runner():
-    pass 
-
+class Runner():
+    def __init__(self, x=0, y=0):
+        self.custome = pygame.image.load("img/2.png")
+        self.position = (x,y)
+        self.name = "Tortugui"
+    def avanzar(self):
+        self.position[0] += random.randint(1,6) 
+        
 class Game():
-    corredores = []
+    runners = []
+    __startLine = -10
+    __finishLine = 620
     
     def __init__(self):
+        # Creacion de atributos
         self.__screen = pygame.display.set_mode((640,480))
+        self.__background = pygame.image.load("img/bg1.png")
         pygame.display.set_caption("Carrera de tortuguas")
-        # Creacion de atributo
-        self.background = pygame.image.load("img/bg1.png")
-        self.runner = pygame.image.load("img/6.png")
         
+        
+        firstRunner = Runner(self.__startLine,240)
+        firstRunner.name = "Speedy Fast"
+        
+        #self.runners = pygame.image.load("img/6.png")
+        self.runners.append(firstRunner)
+        
+        #runners.append(firstRunner)
+        #runners.append(Runner(self.__startLine, 240))
     
         
     def competir(self):
-        x = 0
-        ganador = False
-        while True:
+        # x = 0
+        gameOver = False
+        while not gameOver:
             # Comprobación de los eventos.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    gameOver = True
+                    
             # Refresacar / renderizar la pantalla.
-            self.__screen.blit(self.background, (0,0))
-            self.__screen.blit(self.runner, (x,240))
+            self.__screen.blit(self.__background, (0,0))
+            self.__screen.blit(self.runners[0].custome, self.runners[0].position)
             pygame.display.flip()
             
-            x += 3
-            if x >= 250:
-                ganador = True
+             # x += 3
+             #if x >= 250:
+             #   ganador = True
         
         pygame.quit()
         sys.exit()
         
 if __name__ == '__main__':
     # Inicializamos el paquete
-    pygame.init()
     game = Game()
+    pygame.font.init() # Alternativa para versiones antiguas es: 'pygame.font.init()'
     game.competir()
-    
-    
-    
-    
-    
-    
     
     

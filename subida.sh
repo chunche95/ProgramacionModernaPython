eec2c70b4010c99321f2a74f08ab0894b4bd5b2c
chunche95 <paeste95.pb@gmail.com>
2019-05-21 19:16:33 +0200

    Script automatizacion - version 1.0

--- subida.sh
+++ subida.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# @Title: Subida de cambios a GitHub
+# @Description: Para automatizar la subida, cada 5 min el sistema lanza 
+# el script y realiza una subida de los cambios que encuentra.
+# @Author: Chunche
+
 git status
 
 git add .
@@ -7,3 +12,5 @@ git add .
 git commit -m "$1"
 
 git push origin master
+
+echo -e "\e[33m Archivos Subidos \m"

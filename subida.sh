69aa84026e0c1c98f6ba98179c1593128ac6ec65
chunche95 <paeste95.pb@gmail.com>
2019-05-22 11:56:26 +0200

    Auto Commit - Eject Sys

--- subida.sh
+++ subida.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # @Title: Subida de cambios a GitHub
-# @Description: Para automatizar la subida, cada 5 min el sistema lanza 
+# @Description: Para automatizar la subida, cada 5 min el sistema lanza
 # el script y realiza una subida de los cambios que encuentra.
 # @Author: Chunche
+param=$1
+
 
 echo -e "\e[32m Estado de repositorio... \n"
 git status
@@ -12,7 +14,15 @@ echo -e "\e[33m Añadiendo cambios a zona 'de envio'... "
 git add .
 
 echo -e "\e[34m  Añadiendo comentario a los cambios..."
-git commit -m "$1"
+if [ -z $param ]
+then
+	echo -e "\e[31m Añadiendo comentario automático de sistema..."
+	comentario="Auto Commit - Eject Sys"
+	git commit -m "$comentario"
+else
+	echo -e "\e[30m Añadiendo su comentario"
+	git commit -m "$1"
+fi
 
 echo -e "e[35m Subiendo archivos..."
 git push origin master

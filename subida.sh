67b31489f4eb8a299cff291cfbf3649646e67c8f
chunche95 <paeste95.pb@gmail.com>
2019-05-22 12:40:47 +0200

    Auto Commit - Eject Sys

--- subida.sh
+++ subida.sh
@@ -6,27 +6,38 @@
 # @Author: Chunche
 
 param=$1
+echo "-------------------------------------------"
 echo -e "\e[1;32m Estado de repositorio... \n"
+echo "-------------------------------------------"
 git status
 sleep 3
+echo "-----------------------------------------------------"
 echo -e "\e[1;32m Añadiendo cambios a zona 'de envio'... "
+echo "-----------------------------------------------------"
 git add .
-
+echo "-----------------------------------------------------"
 echo -e "\e[1;32m  Añadiendo comentario a los cambios..."
+echo "-----------------------------------------------------"
 if [ -z $param ]
 then
+	echo "=========================================================="
 	echo -e "\e[0;31m Añadiendo comentario automático de sistema..."
+	echo "=========================================================="
 	comentario="Auto Commit - Eject Sys"
 	git commit -m "$comentario"
 else
+	echo "===================================="
 	echo -e "\e[0;31m Añadiendo su comentario"
+	echo "===================================="
 	git commit -m "$1"
 fi
-
+echo "---------------------------------"
 echo -e "\e[1;32m Subiendo archivos..."
+echo "--------------------------------"
 git push origin master
-
+echo "--------------------------------"
 echo -e "\e[0;36m Archivos Subidos \t"
+echo "--------------------------------"
 sleep 5
 
 #clear

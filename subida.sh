6bce6839f29e161697aa2645380c12c0096d485e
chunche95 <paeste95.pb@gmail.com>
2019-05-22 12:43:49 +0200

    Auto Commit - Eject Sys

--- subida.sh
+++ subida.sh
@@ -7,42 +7,37 @@
 
 param=$1
 echo "-------------------------------------------"
-echo -e "\e[1;37m Estado de repositorio... \n"
+echo -e "\e[1;37m Estado de repositorio... \e"
 echo "-------------------------------------------"
-echo "\n"
 git status
 sleep 3
 echo "-----------------------------------------------------"
-echo -e "\e[1;37m Añadiendo cambios a zona 'de envio'... "
+echo -e "\e[1;37m Añadiendo cambios a zona 'de envio'... \e"
 echo "-----------------------------------------------------"
-echo "\n"
 git add .
 echo "-----------------------------------------------------"
-echo -e "\e[1;37m  Añadiendo comentario a los cambios..."
+echo -e "\e[1;37m  Añadiendo comentario a los cambios...  \e"
 echo "-----------------------------------------------------"
-echo "\n"
 if [ -z $param ]
 then
 	echo "=========================================================="
-	echo -e "\e[0;31m Añadiendo comentario automático de sistema..."
+	echo -e "\e[0;31m Añadiendo comentario automático de sistema... \e"
 	echo "=========================================================="
 	comentario="Auto Commit - Eject Sys"
 	git commit -m "$comentario"
 else
 	echo "===================================="
-	echo -e "\e[0;31m Añadiendo su comentario"
+	echo -e "\e[0;31m Añadiendo su comentario \e"
 	echo "===================================="
 	git commit -m "$1"
 fi
 echo "---------------------------------"
-echo -e "\e[1;37m Subiendo archivos..."
+echo -e "\e[1;37m Subiendo archivos... \e"
 echo "--------------------------------"
-echo "\n"
 git push origin master
 echo "--------------------------------"
-echo -e "\e[1;36m Archivos Subidos \t"
+echo -e "\e[1;36m Archivos Subidos \t \e"
 echo "--------------------------------"
-echo "\n"
 sleep 5
 
 #clear

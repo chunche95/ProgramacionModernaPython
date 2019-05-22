251cc2fd7ba935650c4a02d22bcc259c0a5c1d2b
chunche95 <paeste95.pb@gmail.com>
2019-05-22 11:42:57 +0200

    Añadiendo comentarios a script de subida

--- subida.sh
+++ subida.sh
@@ -5,12 +5,19 @@
 # el script y realiza una subida de los cambios que encuentra.
 # @Author: Chunche
 
+echo -e "\e[32m Estado de repositorio... \n"
 git status
-
+sleep 3
+echo -e "\e[33m Añadiendo cambios a zona 'de envio'... "
 git add .
 
+echo -e "\e[34m  Añadiendo comentario a los cambios..."
 git commit -m "$1"
 
+echo -e "e[35m Subiendo archivos..."
 git push origin master
 
-echo -e "\e[33m Archivos Subidos \m"
+echo -e "\e[36m Archivos Subidos \t"
+sleep 5
+
+clear

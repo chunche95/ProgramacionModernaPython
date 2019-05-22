bd81448743392d57ec4a604c4af19729446e2808
chunche95 <paeste95.pb@gmail.com>
2019-05-22 19:43:22 +0200

    Creacion de mi primer tkinter - Mi primera ventana

---
+++ programacion_moderna_python-chunche/8-TermómetroTkinter/ejemploTkinter.py
@@ -0,0 +1,19 @@
+# @Title: Crear Termómetro Gráfico
+# @Description: Creacion de un termometro con interfaz grafica de usuario, 
+# 
+# @Version: 1.0
+# @Author: Chunche
+#
+
+from tkinter import *
+from tkinter import ttk
+
+# Ventana kinter
+root = Tk()
+# Tamaño de la pantalla
+root.geometry("640x480")
+root.title("Mi primera ventana")
+
+# Creacion d ela ventana
+root.mainloop()
+

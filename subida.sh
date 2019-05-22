500beaeb1566e763e03413b5cfeb2d1c6fd22563
chunche95 <paeste95.pb@gmail.com>
2019-05-22 12:46:12 +0200

    Cambios en colores, comprobacion de comentario almacenado

--- subida.sh
+++ subida.sh
@@ -7,37 +7,44 @@
 
 param=$1
 echo "-------------------------------------------"
-echo -e "\e[1;37m Estado de repositorio... \e"
+echo -e "\e[1;30m Estado de repositorio... \e"
 echo "-------------------------------------------"
+echo ""
 git status
 sleep 3
 echo "-----------------------------------------------------"
-echo -e "\e[1;37m Añadiendo cambios a zona 'de envio'... \e"
+echo -e "\e[1;32m Añadiendo cambios a zona 'de envio'... \e"
 echo "-----------------------------------------------------"
+echo ""
 git add .
 echo "-----------------------------------------------------"
-echo -e "\e[1;37m  Añadiendo comentario a los cambios...  \e"
+echo -e "\e[1;33m  Añadiendo comentario a los cambios...  \e"
 echo "-----------------------------------------------------"
+echo ""
 if [ -z $param ]
 then
 	echo "=========================================================="
 	echo -e "\e[0;31m Añadiendo comentario automático de sistema... \e"
 	echo "=========================================================="
+	echo ""
 	comentario="Auto Commit - Eject Sys"
 	git commit -m "$comentario"
 else
 	echo "===================================="
 	echo -e "\e[0;31m Añadiendo su comentario \e"
 	echo "===================================="
+	echo ""
 	git commit -m "$1"
 fi
 echo "---------------------------------"
-echo -e "\e[1;37m Subiendo archivos... \e"
+echo -e "\e[1;34m Subiendo archivos... \e"
 echo "--------------------------------"
+echo ""
 git push origin master
 echo "--------------------------------"
 echo -e "\e[1;36m Archivos Subidos \t \e"
 echo "--------------------------------"
+echo ""
 sleep 5
 
-#clear
+clear

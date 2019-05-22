0571295c546a11b022e6d86d2ea1a5432b789ccb
chunche95 <paeste95.pb@gmail.com>
2019-05-22 19:59:38 +0200

    Primera clase y definicion de metodos

--- programacion_moderna_python-chunche/8-TermómetroTkinter/ejemploTkinter.py
+++ programacion_moderna_python-chunche/8-TermómetroTkinter/ejemploTkinter.py
@@ -1,6 +1,9 @@
 # @Title: Crear Termómetro Gráfico
 # @Description: Creacion de un termometro con interfaz grafica de usuario, 
-# 
+#
+# Nota: En los proyectos tkinter principalmente suelen poner la palabra 'root' como objeto principal de inicio de los programas,
+# sin embargo, puedo llamarlo como quiera.
+#
 # @Version: 1.0
 # @Author: Chunche
 #
@@ -8,12 +11,27 @@
 from tkinter import *
 from tkinter import ttk
 
-# Ventana kinter
-root = Tk()
-# Tamaño de la pantalla
-root.geometry("640x480")
-root.title("Mi primera ventana")
+#
+# El constructor no necesita el metodo 'self' realmente, pero es recomendable, como una <<Buena práctica de programacion>>, python no necesita este paso
+# pero otros lenguajes de programación si lo necesitan para trabajar.
+# Además, en esta librería por defecto crea un método al que llama 'root' con lo cual, la clase que creamos llama a éste por defecto.
+#
 
-# Creacion d ela ventana
-root.mainloop()
+class mainApp():
+    # Tamaño de la ventana a crear
+    #size = "640x480" # Tamaño por defecto de la pantalla
+    size = "1024x768"
+    def __init__(self):
+        # Ventana kinter
+        self.root = Tk()
+        # Tamaño de la pantalla
+        self.root.geometry(self.size) # Llamo al método size declarado al inicio de la clase.
+        self.root.title("Mi primera ventana")
+        self.root.configure(bg = "lightblue")
+    def start(self):
+        self.root.mainloop()
+if __name__ == '__main__':
+    app = mainApp()
+    # Creacion d ela ventana
+    app.start()
 

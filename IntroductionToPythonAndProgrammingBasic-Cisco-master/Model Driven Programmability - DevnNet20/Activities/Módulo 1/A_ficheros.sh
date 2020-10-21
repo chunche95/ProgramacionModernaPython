ce70329326b1efbf9ff54e4444984758a3ffab88
chunche95 <paeste95.pb@gmail.com>
2020-10-21 21:58:16 +0200

    Módulo 1

---
+++ IntroductionToPythonAndProgrammingBasic-Cisco-master/Model Driven Programmability - DevnNet20/Activities/Módulo 1/A_ficheros.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git add "07_file-access(Create a script to allow user to add devices) - Version 2.py"
+git add "07_file-access(Create a script to allow user to add devices).py"
+git add "07_file-access(remove Blank Lines).py"
+git add "07_file-access.(copy file content into a list variable).py"
+git add "07_file-access.py"
+git add "08_parse_json_1.py"
+git add "08_parse_json_2.py"
+git add "08_parse_json_3.py"
+git add "08_parse_json_4.py"
+git add "08_parse_json_5.py"
+git add "08_parse_json_5_testing_routes.PNG"
+git add "08_parse_json_6.py"
+git add "08_parse_json_7.py"
+git add "08_parse_json_headers2.pdf"
+git add "08_parse_json_route1.json"
+git add "08_parse_json_route2.json"
+git add "A_ficheros.sh"
+git add "devices.txt"
+git add .
+git nota "Módulo 1"
+git sube

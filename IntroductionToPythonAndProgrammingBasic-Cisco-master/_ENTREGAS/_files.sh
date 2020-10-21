2a2395679a4847b1414e19ca96a7cde70e1f65f9
chunche95 <paeste95.pb@gmail.com>
2020-10-21 22:19:26 +0200

    _Entregas prácticas

---
+++ IntroductionToPythonAndProgrammingBasic-Cisco-master/_ENTREGAS/_files.sh
@@ -0,0 +1,10 @@
+
+git add "_files.sh" 
+git add "AutomatizationScriptInfoAPIC-EM.py" 
+git add "calculadora extendida.py" 
+git add "calculadora.py" 
+git add "ConnectRouterCSR1000 .py" 
+git add "path_routes.py" 
+git add "Pruebas  y docs"
+git nota "_Entregas prácticas"
+git sube

2d7a25f176327cec0f495af2dfac4210a5b95398
chunche95 <paeste95.pb@gmail.com>
2020-10-21 22:08:38 +0200

    Mod.2

--- IntroductionToPythonAndProgrammingBasic-Cisco-master/Model Driven Programmability - DevnNet20/Activities/Módulo 2/_fichero.sh
+++ IntroductionToPythonAndProgrammingBasic-Cisco-master/Model Driven Programmability - DevnNet20/Activities/Módulo 2/_fichero.sh
@@ -1,10 +1,5 @@
 
 git add "_fichero.sh" 
-git add "modifyInterfaceConfigurationWithRESConfInPython.py"
-git add "netconf-w-Python-List Capabilities.py" 
-git add "RESTCongWithPostman.py" 
-git add "retrievethsIOS-XE VM's existing running configuration.py" 
-git add "settingUpTheLabEnvironment.py" 
-git add "UpdateConfig-RetrievethsIOS-XE VM's existing running configuration.py"
+git add "UpdateConfig-RetrievethsIOS-XEVM'sexistingrunningconfig.py"
 git nota "Mod.2"
 git sube

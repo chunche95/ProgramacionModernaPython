34826faec5eba77d9c8839f19d381489a259e672
chunche95 <paeste95.pb@gmail.com>
2021-01-04 23:28:37 +0100

    Python & BDs

---
+++ IntroductionToPythonAndProgrammingBasic-Cisco-master/PrimaryKeys.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+conexion = sqlite3.connect("Productos.db") # Establecemos conexion
+
+# Insertamos datos en la tabla SQL
+cursor = conexion.cursor();
+
+cursor.execute('''
+DROP TABLE PRODUCTOS
+CREATE TABLE PRODUCTOS(
+    CODIGO_P VARCHAR(10) PRIMARY KEY,
+    NOMBRE_P VARCHAR(50),
+    SECCION_P VARCHAR(20)
+)
+
+''')
+
+productos=[
+    ('ART.1', 'Leche', 'Lacteos'),
+    ('ART.2', 'Arroz', 'Cereales'),
+    ('ART.3', 'Manzana Golden', 'Frutas y Verduras'),
+    ('ART.4', 'Cerveza', 'Bebidas'),
+    
+]
+
+cursor.executemany("INSERT INTO PRODUCTOS VALUES(?,?,?)", productos)
+
+
+conexion.commit();
+conexion.close(); # Desconectamos
